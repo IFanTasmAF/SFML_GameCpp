bb5dd2cc4be7e8ebaf7e77ed700d004e2da09d91
IFanTasmAF <akhmetov129@gmail.com>
2026-04-09 15:45:01 +0300

    Улучшения

--- entities/Player.cpp
+++ entities/Player.cpp
@@ -1,17 +1,48 @@
 #include "Player.h"
 
+#include <cmath>
+
 Player::Player(float x, float y) : Entity(x, y) {
   shape.setSize(sf::Vector2f(50.f, 50.f));
   shape.setFillColor(sf::Color::Green);
 }
 
 void Player::update(float dt) {
-  float speed = 200.f;
+  sf::Vector2f input(0.f, 0.f);
 
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) shape.move(0, -speed * dt);
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) shape.move(0, speed * dt);
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) shape.move(-speed * dt, 0);
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) shape.move(speed * dt, 0);
+  // ввод
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) input.y -= 1.f;
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) input.y += 1.f;
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) input.x -= 1.f;
+  if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) input.x += 1.f;
+
+  // нормализация (чтобы диагональ не была быстрее)
+  if (input.x != 0.f || input.y != 0.f) {
+    float length = std::sqrt(input.x * input.x + input.y * input.y);
+    input /= length;
+
+    velocity += input * acceleration * dt;
+  } else {
+    // трение (замедление)
+    if (velocity.x > 0.f)
+      velocity.x = std::max(0.f, velocity.x - friction * dt);
+    else if (velocity.x < 0.f)
+      velocity.x = std::min(0.f, velocity.x + friction * dt);
+
+    if (velocity.y > 0.f)
+      velocity.y = std::max(0.f, velocity.y - friction * dt);
+    else if (velocity.y < 0.f)
+      velocity.y = std::min(0.f, velocity.y + friction * dt);
+  }
+
+  // ограничение скорости
+  float speed = std::sqrt(velocity.x * velocity.x + velocity.y * velocity.y);
+  if (speed > maxSpeed) {
+    velocity = (velocity / speed) * maxSpeed;
+  }
+
+  // движение
+  shape.move(velocity * dt);
 }
 
 void Player::draw(sf::RenderWindow& window) { window.draw(shape); }
--- entities/Player.h
+++ entities/Player.h
@@ -2,6 +2,13 @@
 #include "Entity.h"
 
 class Player : public Entity {
+ private:
+  sf::Vector2f velocity;
+
+  float acceleration = 800.f;
+  float maxSpeed = 300.f;
+  float friction = 600.f;
+
  public:
   Player(float x, float y);
 
